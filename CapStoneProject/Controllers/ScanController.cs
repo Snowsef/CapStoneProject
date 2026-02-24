f8d2450fee5623be107c0ecc39bee6fbe8621f26
Joey Snow <joey.snow.98@gmail.com>
2026-02-24 16:29:48 -0500

    Final Polish, Security Updates, and README

--- CapStoneProject/Controllers/ScanController.cs
+++ CapStoneProject/Controllers/ScanController.cs
@@ -1,11 +1,12 @@
-﻿using CapStoneProject.Data;   
-using CapStoneProject.Models; 
+﻿using CapStoneProject.Data;
+using CapStoneProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
-
+using Microsoft.AspNetCore.Authorization; 
 namespace CapStoneProject.Controllers
 {
+    [Authorize] 
     public class ScanController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -22,18 +23,15 @@ namespace CapStoneProject.Controllers
 
         public async Task<IActionResult> History(string searchString)
         {
-           
             var logs = from l in _context.ScanLogs
                        select l;
 
-           
             if (!String.IsNullOrEmpty(searchString))
             {
-                              
                 logs = logs.Where(s => s.TargetIPAddress.Contains(searchString)
                                     || s.OpenPortsDetected.Contains(searchString));
             }
-                       
+
             return View(await logs.OrderByDescending(s => s.ScanTimestamp).ToListAsync());
         }
     }
